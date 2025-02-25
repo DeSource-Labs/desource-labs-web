ef2bec77325a90de8de79a7c4b2ece46f948615f
Stefan Popov <stef4n.popov@gmail.com>
2025-02-25 01:17:21 +0100

    Update meta.ts

--- meta.ts
+++ meta.ts
@@ -4,7 +4,7 @@ export const meta = {
   title: 'DeSource Labs — Engineering beyond limits',
   description: 'DeSource Labs is a software engineering company that specializes in building custom software solutions for businesses and startups.',
   keywords: 'software engineering, blockchain, web3, telegram mini apps, front-end development, back-end development, ux, user experience, software development, software solutions, software company, software development company, software engineering company, software development services, software development agency, software engineering services, software engineering agency',
-  ogImage: `${process.env.NUXT_PUBLIC_BASE_URL || ''}/og.jpg`,
+  ogImage: 'https://desource-labs.org/og.jpg',
   ogImageWidth: '1200',
   ogImageHeight: '630',
   x: '@desource-labs',
