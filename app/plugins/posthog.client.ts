37c3f0a4c69ce16efdc3a12d26afe595f70f2b1a
Stefan Popov <stef4n.popov@gmail.com>
2025-12-25 02:14:33 +0100

    refactor: Remove runtimeConfig from nuxt config

--- app/plugins/posthog.client.ts
+++ app/plugins/posthog.client.ts
@@ -1,10 +1,10 @@
-import posthog, { type ConfigDefaults } from 'posthog-js';
+import posthog from 'posthog-js';
 
 export default defineNuxtPlugin(nuxtApp => {
-  const runtimeConfig = useRuntimeConfig();
-  const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
-    api_host: runtimeConfig.public.posthogHost,
-    defaults: runtimeConfig.public.posthogDefaults as ConfigDefaults,
+  const posthogPublicKey = import.meta.env.VITE_POSTHOG_PK;
+  const posthogClient = posthog.init(posthogPublicKey, {
+    api_host: '/api/_ph',
+    defaults: '2025-11-30',
     disable_compression: true,
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
     loaded: (posthog) => {
