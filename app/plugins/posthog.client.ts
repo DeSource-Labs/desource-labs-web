706396f6d6565eaa922961dc0b358054a776f12b
Stefan Popov <stef4n.popov@gmail.com>
2026-02-16 01:51:29 +0100

    feature: Update posthog defaults

--- app/plugins/posthog.client.ts
+++ app/plugins/posthog.client.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin(nuxtApp => {
   const posthogPublicKey = import.meta.env.VITE_POSTHOG_PK;
   const posthogClient = posthog.init(posthogPublicKey, {
     api_host: '/api/_ph',
-    defaults: '2025-11-30',
+    defaults: '2026-01-30',
     disable_compression: true,
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
     loaded: (posthog) => {
